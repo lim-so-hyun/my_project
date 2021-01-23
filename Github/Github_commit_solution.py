e6f813479aff6519a4c8d6bd52938d9f7463897a
lim-so-hyun <76216512+lim-so-hyun@users.noreply.github.com>
2021-01-23 15:28:03 +0900

    Initial commit

---
+++ Github/Github_commit_solution.py
@@ -0,0 +1,21 @@
+# Commit Practice
+
+# 첫 Commit 연습용 text file
+
+# Pull 에 필요한 명령어) git pull origin master
+# Push 에 필요한 명령어) git status / git add . / git commit -m "message" / git push origin +master
+
+# Self commit practice
+
+이미 연결을 해둔 상태에서는, 다시 연결하는 작업은 필요 없음.
+폴더 내의 파일에 수정사항 생기면 다시 tracking 해주어야 하는데, 이제 Git Bash 열고 git status 부터 시작하면 된다!
+
+1) git status 입력
+- tracking 해야할 변경 사항 수 뜸
+2) git add .
+- 모든 수정 사항 tracking
+3) git commit -m "second commit"
+- 어떤 message로 commit 올릴 건지 설정
+4) git push origin master
+4-1) user name 입력
+4-2) password 입력
