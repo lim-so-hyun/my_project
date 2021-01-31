79244ecf9055296c08f60e4bad491a009b1abb47
lim-so-hyun <76216512+lim-so-hyun@users.noreply.github.com>
2021-01-31 18:11:12 +0900

    20210131_tab_to_4
    tabto4 수행한 결과 보여주게 하는 파일이 tabto4.py
    근데 이렇게 하면 tab이랑 공백 4개인것 구분 어려우니까 a라는 text 파일에 있던 tab을 공백 4개로 바꾼 결과를 다시 b라는 text 파일 만들어서 write 하도록 수정한 파일이 tabto4_show.py

---
+++ Programms/tabto4.py
@@ -0,0 +1,13 @@
+# C:/doit/tabto4.py
+
+import sys
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+f = open(src)
+tab_content = f.read()
+f.close()
+
+space_content = tab_content.replace("\t", " "*4)
+print(space_content)
